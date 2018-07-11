fa8499fea24742138baa51803d13a17ea75bf010
정선우 <jsw2701@gieun-MacBook.local>
2018-07-11 18:14:01 +0900

    track 테이블 생성 및 csv파일 삽입

--- README.md
+++ README.md
@@ -6,3 +6,14 @@ bundle exec figaro install   # => .gitignore 생성 및 application.yml에 키 �
 rake db:migrate
 ```
 1. user 테이블 수정 - 선우
+2. track 테이블 생성 및 데이터 추가
+```console
+rails g model track track:string   # => 원하는 컬럼의 자료타입을 선정해서 생성
+rake db:migrate # => db를 생성해서 스키마에 반영하기위한 명령어
+# DB값을 계속 drop 및 migrate를 하기 때문에 csv파일에 저장해서 업로드 하는게 편할거 같음
+csv 파일 반영 방법
+- csv파일 삽입 db폴더 바로 밑에(csv파일의 경우 구글 스프레드시트로 작성해야 인코딩 에러없음)
+- 입력하는 코드 seeds.rb에 삽입
+- console에서 rake db:seed 로 삽입
+- 반영 완료
+```
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180710042538) do
+ActiveRecord::Schema.define(version: 20180711082257) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "content"
@@ -45,6 +45,12 @@ ActiveRecord::Schema.define(version: 20180710042538) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "img"
+  end
+
+  create_table "tracks", force: :cascade do |t|
+    t.string   "track"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
