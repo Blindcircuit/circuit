36d01ffd7989242344e9a7e4e9ff8e4977db6d89
AnabolicAction <itw9622@gmail.com>
2018-07-19 08:58:12 +0000

    record 페이지추가

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
   root 'circuit#index'
   get 'circuit/index'
   get 'user/mypage'
+  get 'circuit/record'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
