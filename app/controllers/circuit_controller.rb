4379868b01376f52907b8552e2b505cf7de1cb90
정선우 <greentarte@naver.com>
2018-07-23 14:49:04 +0900

    기록정보 아이디 중복 제거

--- app/controllers/circuit_controller.rb
+++ app/controllers/circuit_controller.rb
@@ -4,9 +4,22 @@ class CircuitController < ApplicationController
   end
 
   def record
-    @yongin=Record.where(track_id: 1).order(laptime: :ASC).page(params[:page]).per(10)
-    @yeongam=Record.where(track_id: 3).order(laptime: :ASC).page(params[:page]).per(10)
-    @inje=Record.where(track_id: 2).order(laptime: :ASC).page(params[:page]).per(10)
+    if params[:car] && params[:model] && params[:track]
+      @yongin=Record.where(track_id: params[:track], car_id: params[:car]).order(laptime: :ASC).page(params[:page]).per(10)
+    else
+      @yongin = Record.where(track_id: 1).order(laptime: :ASC).group_by(&:user_id).map{|key, val| val.first}
+      @yongin = Kaminari.paginate_array(@yongin).page(params[:page]).per(10)
+
+      @yeongam = Record.where(track_id: 3).order(laptime: :ASC).group_by(&:user_id).map{|key, val| val.first}
+      @yeongam = Kaminari.paginate_array(@yeongam).page(params[:page]).per(10)
+
+      @inje = Record.where(track_id: 2).order(laptime: :ASC).group_by(&:user_id).map{|key, val| val.first}
+      @inje = Kaminari.paginate_array(@inje).page(params[:page]).per(10)
+    end
+
+
+
+    @tracks=Track.all
   end
 
 end
